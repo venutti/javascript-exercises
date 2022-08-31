29a766166830b3c1c8290eefd3680fa3fd20a324
venutti <lvenutti@fi.uba.ar>
2022-08-31 19:32:42 -0300

    Resuelvo ejercicio 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const round = function(number, decimals) {
+  return Math.round((number + Number.EPSILON) * (10 ** decimals)) / (10 ** decimals);
 };
 
-const ctof = function() {
+const ftoc = function(far) {
+  let celsuis = (far - 32) * 5 / 9;
+  return round(celsuis, 1);
+};
 
+const ctof = function(celsius) {
+  let far = (celsius * 9 / 5) + 32;
+  return round(far, 1);
 };
 
 // Do not edit below this line
