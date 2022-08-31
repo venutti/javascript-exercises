e75ec038cdeeb9b9bfc94859051e8d29b1eab693
venutti <lvenutti@fi.uba.ar>
2022-08-31 12:21:29 -0300

    Resuelvo ejercicio 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    let i = string.length - 1;
+    while (i >= 0) {
+        reversed += string[i];
+        i--;
+    }
+    return reversed;
 };
 
 // Do not edit below this line
