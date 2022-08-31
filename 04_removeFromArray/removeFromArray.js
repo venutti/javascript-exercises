a46f46fa512e4e74af76d500feacfc1c505f8d22
venutti <lvenutti@fi.uba.ar>
2022-08-31 13:00:44 -0300

    Resuelvo ejercicio 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const getIndex = function(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] === element) {
+            return i;
+        }
+    }
+};
 
+const removeFromArray = function(array, ...elemRemove) {
+    for(const element of elemRemove) {
+        let i = getIndex(array, element);
+        if(i === undefined) {
+            continue;
+        }
+        array.splice(i, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
