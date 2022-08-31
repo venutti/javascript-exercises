830c75b20cd53f3e0976f38e3b0377406d8a503a
venutti <lvenutti@fi.uba.ar>
2022-08-31 12:00:10 -0300

    Resuelvo ejercicio 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    let repeatedString = '';
+    for (let i = 0; i < times; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
