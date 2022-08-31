f82bec3976caf81bcedf8535d541fe96f2789eda
venutti <lvenutti@fi.uba.ar>
2022-08-31 19:55:23 -0300

    Arreglo ejercicio 4
    Porque el código anterior modificaba el array recibido por parámetro, cosa que está mal en programación.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,12 @@
-const getIndex = function(array, element) {
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] === element) {
-            return i;
-        }
-    }
-};
 
 const removeFromArray = function(array, ...elemRemove) {
-    for(const element of elemRemove) {
-        let i = getIndex(array, element);
-        if(i === undefined) {
-            continue;
+    const newArray = [];
+    for(const element of array) {
+        if(!elemRemove.includes(element)) {
+            newArray.push(element);
         }
-        array.splice(i, 1);
     }
-    return array;
+    return newArray;
 };
 
 // Do not edit below this line
