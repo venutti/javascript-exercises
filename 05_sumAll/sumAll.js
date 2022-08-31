150aec72f6ac60488e2c7e8e749596030f460b93
venutti <lvenutti@fi.uba.ar>
2022-08-31 13:48:33 -0300

    Resuelvo ejercicio 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof a !== 'number' || typeof b !==  'number') {
+        return 'ERROR';
+    }
+    if(a < 0 || b < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
 
+    let min, max;
+    if(a < b) {
+        min = a;
+        max = b;
+    }else{
+        min = b;
+        max = a;
+    }
+
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
