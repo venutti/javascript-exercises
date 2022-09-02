4f3d83c7e689f06f03dbf574f7c472bd637c35c7
venutti <lvenutti@fi.uba.ar>
2022-09-02 20:26:10 -0300

    Resuelvo ejercicio 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arrAlpha = string.toLowerCase().split("").filter(word => word.match(/[a-z]/));
+    const reversed  = [...arrAlpha];
+    reversed.reverse();
+    return arrAlpha.join("") === reversed.join("");
 };
 
 // Do not edit below this line
