4e9339ca1c3e79b0fe2d36ff21b465b4b079c948
venutti <lvenutti@fi.uba.ar>
2022-09-02 19:59:03 -0300

    Resuelvo ejercicio 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum,elem) => sum+=elem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prod, elem) => prod *= elem, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let prod = 1;
+  for(let i = 1; i <= n; i++) {
+    prod *= i;
+  }
+  return prod;
 };
 
 // Do not edit below this line
