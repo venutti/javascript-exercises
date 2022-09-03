78bf0a317b443cf26bf08bda5a894e73797f9b59
venutti <lvenutti@fi.uba.ar>
2022-09-02 21:42:59 -0300

    Resuelvo ejercicio 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // recibe un array de objetos
+    const sortedPeople = people.sort((a, b) => {
+        const aYearOfDeath = a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear();
+        const bYearOfDeath = b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear();
+        const aOld = aYearOfDeath - a.yearOfBirth;
+        const bOld = bYearOfDeath - b.yearOfBirth;
+        return (aOld > bOld) ? -1 : 1;
+    })
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
