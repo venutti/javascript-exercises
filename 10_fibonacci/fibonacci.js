b8e6ff166dee35697e7b3a5ec01483492721b043
venutti <lvenutti@fi.uba.ar>
2022-09-02 21:20:28 -0300

    Resuelvo ejercicio 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = +number;
+    if ( number <= 0) {
+        return "OOPS";
+    }
+    if ( number === 1 || number === 2 ) {
+        return 1;
+    }
+    return fibonacci( number - 1 ) + fibonacci( number - 2 );
 };
 
 // Do not edit below this line
