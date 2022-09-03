b3da3c4f419660a6512c837e3397dca392e413e6
venutti <lvenutti@fi.uba.ar>
2022-09-02 21:25:44 -0300

    Resuelvo ejercicio 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    //recibe un array de objetos
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
